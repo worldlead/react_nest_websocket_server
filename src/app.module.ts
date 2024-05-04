f75e6a4481e35ee17a84db5f44bbdca81a8e6610
worldlead <procosep@gmail.com>
2024-05-03 21:13:51 -0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ChatModule } from './chat/chat.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ChatModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'client'),
+    }),
+  ],
+})
+export class AppModule {}
