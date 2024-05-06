fee92d489c2ec329057b921992b4f873a45af15f
worldlead <procosep@gmail.com>
2024-05-06 08:22:28 -0700

    feat: added methods to handle connection and disconnection to chat.gateway class

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,11 +1,20 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { 
+  MessageBody, 
+  SubscribeMessage, 
+  WebSocketGateway, 
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { 
   ServerToClientEvents,
   ClientToServerEvents,
-  Message
+  Message,
+  User
 } from 'src/types/chat.interface';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { UserService } from 'src/user/user.service';
 
 @WebSocketGateway({
   cors: {
@@ -13,7 +22,10 @@ import { Server } from 'socket.io';
   },
 })
 
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  constructor(private userService: UserService) {}
+
   @WebSocketServer() server: Server = new Server<
     ServerToClientEvents,
     ClientToServerEvents
@@ -28,7 +40,31 @@ export class ChatGateway {
     payload: Message,
   ): Promise<Message> {
     this.logger.log(payload);
-    this.server.emit('chat', payload);
+    this.server.to(payload.roomName).emit('chat', payload) // broadcast messsages
     return payload;
+  }
+
+  @SubscribeMessage('join_room')
+  async handleSetClientDataEvent(
+    @MessageBody()
+    payload: {
+      roomName: string
+      user: User
+    }
+  ) {
+    if (payload.user.socketId) {
+      this.logger.log(`${payload.user.socketId} is joining ${payload.roomName}`)
+      await this.server.in(payload.user.socketId).socketsJoin(payload.roomName)
+      await this.userService.addUserToRoom(payload.roomName, payload.user)
+    }
+  }
+
+  async handleConnection(socket: Socket): Promise<void> {
+    this.logger.log(`Socket connected: ${socket.id}`)
+  }
+
+  async handleDisconnect(socket: Socket): Promise<void> {
+    await this.userService.removeUserFromAllRooms(socket.id)
+    this.logger.log(`Socket disconnected: ${socket.id}`)
   }
 }
--- src/types/chat.interface.ts
+++ src/types/chat.interface.ts
@@ -1,12 +1,21 @@
 export interface User {
     userId: string;
     userName: string;
+    socketId: string;
   }
+
+  export interface Room {
+    name: string
+    host: User
+    users: User[]
+  }
+
   
   export interface Message {
     user: User;
     timeSent: string;
     message: string;
+    roomName: string;
   }
   
   // Interface for when server emits events to clients.
@@ -17,4 +26,5 @@ export interface User {
   // Interface for when clients emit events to the server.
   export interface ClientToServerEvents {
     chat: (e: Message) => void;
+    join_room: (e: { user: User; roomName: string }) => void
   }
