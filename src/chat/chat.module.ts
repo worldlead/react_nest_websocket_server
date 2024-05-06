9de3035768d6369859a88e8a809a318112c65530
worldlead <procosep@gmail.com>
2024-05-06 11:58:21 -0700

    fix: added cors

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ChatGateway } from "./chat.gateway";
+import { UserModule } from "src/user/user.module";
 
 @Module({
+    imports: [UserModule],
     providers: [ChatGateway],
+    
 })
 
 export class ChatModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors()
   await app.listen(3001);
 
   app.enableShutdownHooks();
